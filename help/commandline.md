4b4e9c80ac18038b54e0c31299e578349bbd6cad
Ben Taylor <ben.taylor.uk@gmail.com>
2014-05-28 22:53:17 +0100

    Initial stab at #438.  Add --fsiargs switch to fake.exe so one can fully specify the command line used by FAKE to invoke FSI.  I required this as my build script uses UnionArgParser and there was no way for me to provide arguments to my build script in the format that I wanted.  I hope this will be useful in other scenarios (like for those wishing to specify multiple FSI options). * New switch implemented and previous usage left intact. * Command line documentation written for existing switches and new usage. * Add FsiArgs DU for more specific configuration of FSI invocation.

--- help/commandline.md
+++ help/commandline.md
@@ -1,3 +1,68 @@
+# FAKE build command line usage
+
+You can run FAKE from the command line in two ways.
+
+The first usage is as follows:
+
+    fake.exe [<buildScriptPath>] [<targetName>] [details] [<varialble1>[=<value1>] ... <variableN>[=<valueN>]] [-d:<string>]
+
+The second usage is intended for builders that want full control of the arguments FAKE.exe provides to FSI when running the build script.
+
+    fake.exe [<targetName>] [details] [<varialble1>[=<value1>] ... <variableN>[=<valueN>]] [--fsiScriptArgs <arg1> [... <argN>]]
+
+## buildScriptPath
+
+Path to the `fsx` build script file that FAKE will run in an `fsi` session.
+
+Optional.  If not specified, FAKE will either use the first `.fsx` file it finds in the working directory.
+
+**Note:  If you are using `--fsiargs`, you must not provide this argument.** 
+
+## &lt;targetName>
+
+Specify the name of the single Target you wish to run.
+
+Optional.  If you do not specify a Target, the build script will control which Targets are run.
+
+## details
+
+TBC
+
+## &lt;variableN>[=&lt;valueN>]
+
+Specify key value pairs (or just keys) that will be set on the `ProcessStartInfo.EnvironmentVariables` dictionary.  If you specify the variable key only, the value will be set to `true`.
+
+Optional.  
+
+## -d:&lt;string>
+
+Specify a value for the FSI `-d:<string>` option.
+
+## --fsiargs &lt;arg1> .. &lt;argn>
+
+Specify the FSI arguments that FAKE should use to run FSI.  FSI command line usage is as follows:
+
+    fsi.exe [options] [script-file [arguments]]
+
+When using this switch, you must do the following:
+
+* Include your build script path as one of the arguments.
+* Make sure you only have FSI option switches preceding the script path argument (they all start with - or --).
+
+For example:
+
+    fake.exe --fsiargs --warnaserror+ buildstuff.fsx argForBuildScript
+
+
+# FAKE version command line usage
+
+Check the FAKE version with the following usage:
+
+`fake.exe version`
+
+If you use `version` any other arguments you provide will be ignored.
+
+
 # Running FAKE targets from the command line
 
 For this short sample we assume you have the latest version of FAKE in *./tools/*. Now consider the following small FAKE script:
--- src/app/FAKE/Program.fs
+++ src/app/FAKE/Program.fs
@@ -2,6 +2,8 @@
 open Fake
 open System.IO
 
+let fsiArgsSwitch = "--fsiargs"
+
 let printVersion() =
     traceFAKE "FakePath: %s" fakePath
     traceFAKE "%s" fakeVersionStr
@@ -34,25 +36,44 @@ try
     try
         AutoCloseXmlWriter <- true
         let cmdArgs = System.Environment.GetCommandLineArgs()
-        if containsParam "version" cmdArgs then printVersion() else
-        if (cmdArgs.Length = 2 && paramIsHelp cmdArgs.[1]) || (cmdArgs.Length = 1 && List.length buildScripts = 0) then CommandlineParams.printAllParams() else
-        match Boot.ParseCommandLine(cmdArgs) with
-        | None ->
-            let buildScriptArg = if cmdArgs.Length > 1 && cmdArgs.[1].EndsWith ".fsx" then cmdArgs.[1] else Seq.head buildScripts
-            let fakeArgs = cmdArgs |> Array.filter (fun x -> x.StartsWith "-d:" = false)
-            let fsiArgs = cmdArgs |> Array.filter (fun x -> x.StartsWith "-d:") |> Array.toList
-            let args = CommandlineParams.parseArgs (fakeArgs |> Seq.filter ((<>) buildScriptArg) |> Seq.filter ((<>) "details"))
-
-            traceStartBuild()
-            let printDetails = containsParam "details" cmdArgs
-            if printDetails then
-                printEnvironment cmdArgs args
-            if not (runBuildScript printDetails buildScriptArg fsiArgs args) then
-                Environment.ExitCode <- 1
+        if containsParam "version" cmdArgs then printVersion()
+        else
+            if (cmdArgs.Length = 2 && paramIsHelp cmdArgs.[1]) || (cmdArgs.Length = 1 && List.length buildScripts = 0) then CommandlineParams.printAllParams()
             else
-                if printDetails then log "Ready."
-        | Some handler ->
-            handler.Interact()
+                match Boot.ParseCommandLine(cmdArgs) with
+                | None ->
+                    //We support two models.
+                    let oldFsiOptions = cmdArgs |> Array.filter (fun x -> x.StartsWith "-d:") |> Array.toList
+                    let fakeArgs, fsiArgs = 
+                        let fsiArgsSwitchIndex = cmdArgs |> Array.tryFindIndex (fun arg -> toLower arg = "--fsiargs")
+                        match fsiArgsSwitchIndex with
+                        | None ->
+                            //This is the "fake.exe build.fsx target".
+                            let buildScriptArg = if cmdArgs.Length > 1 && cmdArgs.[1].EndsWith ".fsx" then cmdArgs.[1] else Seq.head buildScripts
+                            let fakeArgs = cmdArgs |> Array.filter (fun x -> x.StartsWith "-d:" = false || x.Equals(buildScriptArg, StringComparison.InvariantCultureIgnoreCase))
+                            fakeArgs, FsiArgs(oldFsiOptions, buildScriptArg, []) 
+                        | Some(i) ->
+                            //This is the "fake.exe target --fsiargs --define:MONO build.fsx arg1 arg2".
+                            let fsiArgs = cmdArgs.[i..]
+                            if fsiArgs.Length = 1 then failwith "--fsiargs switch is present, but it is not followed by any arguments."
+                            if oldFsiOptions.Length > 0 then
+                                failwith "-d:<string> fsi option should not be used when also using --fsiargs.  Use -d:<string> after the --fsiargs switch."
+                            match FsiArgs.parse fsiArgs.[1..] with
+                            | Choice1Of2(fsiArgs) -> cmdArgs.[..i-1], fsiArgs
+                            | Choice2Of2(errMsg) -> failwith errMsg
+
+                    let args = CommandlineParams.parseArgs (fakeArgs |> Seq.filter ((<>) "details"))
+
+                    traceStartBuild()
+                    let printDetails = containsParam "details" cmdArgs
+                    if printDetails then
+                        printEnvironment cmdArgs args
+                    if not (runBuildScriptWithFsiArgsAt "" printDetails fsiArgs args) then
+                        Environment.ExitCode <- 1
+                    else
+                        if printDetails then log "Ready."
+                | Some handler ->
+                    handler.Interact()
     with
     | exn -> 
         if exn.InnerException <> null then
